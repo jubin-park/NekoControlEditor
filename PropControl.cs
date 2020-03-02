25d7090638b15aa325b2cded7d93503136e44f1a
Jubin Park <jubin-park@naver.com>
2020-03-02 22:30:36 +0900

    Add PropControl* classes

---
+++ PropControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NekoControlEditor
+{
+    class PropControl
+    {
+        public EKeys Key { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public byte Opacity { get; set; }
+        public bool Visible { get; set; }
+        public bool RectTouchable { get; set; }
+        public uint Width { get; }
+        public uint Height { get; }
+
+        public PropControl()
+        {
+            Key = EKeys.EMPTY;
+            X = 0;
+            Y = 0;
+            Z = 1;
+            Opacity = 255;
+            Visible = true;
+            RectTouchable = false;
+        }
+    }
+}
---
+++ PropControlDirection4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NekoControlEditor
+{
+    class PropControlDirection4 : PropControl
+    {
+        public string BitmapDefault { get; set; }
+        public string BitmapDown { get; set; }
+        public string BitmapLeft { get; set; }
+        public string BitmapRight { get; set; }
+        public string BitmapUp { get; set; }
+        public string BitmapStick { get; set; }
+        public ushort StickMovableRadius { get; set; }
+    }
+}
