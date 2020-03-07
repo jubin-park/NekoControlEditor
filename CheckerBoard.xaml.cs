a3da9ff6d3f3e139f17ba620dc019d0bee958d26
Jubin Park <jubin-park@naver.com>
2020-03-07 23:48:24 +0900

    Delete old code
    Use MVVM

--- CheckerBoard.xaml.cs
+++ CheckerBoard.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,62 @@ namespace NekoControlEditor
     /// <summary>
     /// CheckerBoard.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class CheckerBoard : UserControl
+    public partial class CheckerBoard : UserControl, INotifyPropertyChanged
     {
         public CheckerBoard()
         {
             InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public double mBorderWidth;
+        public double BorderWidth
+        {
+            get
+            {
+                return mBorderWidth + 2;
+            }
+            set
+            {
+                if (mBorderWidth != value)
+                {
+                    mBorderWidth = value;
+                    notifyPropertyChanged("BorderWidth");
+                }
+            }
+        }
+
+        public double mBorderHeight;
+        public double BorderHeight
+        {
+            get
+            {
+                return mBorderHeight + 2;
+            }
+            set
+            {
+                if (mBorderHeight != value)
+                {
+                    mBorderHeight = value;
+                    notifyPropertyChanged("BorderHeight");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            BorderWidth = ActualWidth;
+            BorderHeight = ActualHeight;
+        }
+
+        private void notifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
