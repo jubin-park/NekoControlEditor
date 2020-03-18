2078fdc14f6d41a899365e9c48c8bcb614bd9384
Jubin Park <jubin-park@naver.com>
2020-03-18 13:51:41 +0900

    Add TextValue

---
+++ TextValue.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace NekoControlEditor
+{
+    public class TextValue<T> : INotifyPropertyChanged
+    {
+        private T mValue;
+        public T NowValue
+        {
+            get
+            {
+                return mValue;
+            }
+            set
+            {
+                mValue = value;
+                notifyPropertyChanged("NowValue");
+            }
+        }
+
+        public TextValue(T value)
+        {
+            NowValue = value;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void notifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
