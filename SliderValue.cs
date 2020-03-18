106c4fc01dbcacc812d37b46eb651e0aa6902c1f
Jubin Park <jubin-park@naver.com>
2020-03-18 13:51:31 +0900

    Add SliderValue

---
+++ SliderValue.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace NekoControlEditor
+{
+    public class SliderValue<T> : INotifyPropertyChanged
+    {
+        private T mValue;
+        public T NowValue
+        {
+            get
+            {
+                return mValue;
+            }
+            set
+            {
+                mValue = value;
+                notifyPropertyChanged("NowValue");
+            }
+        }
+        public T Max { get; set; }
+        public T Min { get; set; }
+        public T Step { get; set; }
+
+        public SliderValue(T value, T min, T max, T step)
+        {
+            NowValue = value;
+            Min = min;
+            Max = max;
+            Step = step;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void notifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
